66c627b9930342d19ab26778cae0beef3b72d233
User <User@DESKTOP-NFJOD61.Home>
2022-03-28 17:40:48 +0200

    Versione 1

---
+++ src/main/java/it/polito/tdp/corsi/model/Corso.java
@@ -0,0 +1,77 @@
+package it.polito.tdp.corsi.model;
+
+import java.util.Objects;
+
+public class Corso {
+	
+	private String codins;
+	private Integer crediti;
+	private String nome;
+	private Integer db;
+	
+	public Corso(String codins, Integer crediti, String nome, Integer db) {
+		super();
+		this.codins = codins;
+		this.crediti = crediti;
+		this.nome = nome;
+		this.db = db;
+	}
+
+	public String getCodins() {
+		return codins;
+	}
+
+	public void setCodins(String codins) {
+		this.codins = codins;
+	}
+
+	public Integer getCrediti() {
+		return crediti;
+	}
+
+	public void setCrediti(Integer crediti) {
+		this.crediti = crediti;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getDb() {
+		return db;
+	}
+
+	public void setDb(Integer db) {
+		this.db = db;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codins);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Corso other = (Corso) obj;
+		return Objects.equals(codins, other.codins);
+	}
+
+	@Override
+	public String toString() {
+		return "Corso [codins=" + codins + ", crediti=" + crediti + ", nome=" + nome + ", db=" + db + "]";
+	}
+
+
+
+	
+}
